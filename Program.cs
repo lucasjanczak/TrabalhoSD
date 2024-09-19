09914a485287b2ec416c779fcd1519f4b0432d15
Lucas Janczak <27360875+lucasjanczak@users.noreply.github.com>
2024-09-19 19:48:01 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors("AllowAll");
 
 // Ensure HTTPS redirection is enabled
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
