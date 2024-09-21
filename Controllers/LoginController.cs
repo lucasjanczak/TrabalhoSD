9072176c11908266d741067e30792020096a953e
Lucas Janczak <lucas.janczak2@gmail.com>
2024-09-21 12:07:51 -0300

    Temporarily removed LoginController

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -4,20 +4,21 @@ using Web.Models;
 
 namespace Web.Controllers
 {
-    [ApiController]
-    [Route("login")]
-    public class LoginController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+    //[ApiController]
+    //[Route("login")]
+    //public class LoginController : Controller
+    //{
+        
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
 
 
-        //[HttpPost]
-        //public String Post(msg)
-        //{
+    //    [HttpPost]
+    //    public String Post(msg)
+    //    {
 
-        //}
-    }
+    //    }
+    //}
 }
